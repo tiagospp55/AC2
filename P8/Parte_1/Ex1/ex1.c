d8a4f5189e12fc13f3aef498cd2e6fcd85e4fa3c
Tiago Silva Pereira <tiagosilvapereira55@gmail.com>
2022-06-09 18:15:17 +0100

    INTx

--- P8/Parte_1/Ex1/ex1.c
+++ P8/Parte_1/Ex1/ex1.c
@@ -2,7 +2,7 @@
 
 int main(void){
     T3CONbits.TCKPS =  7;  // 1:256 -> Fout_prescaler = 778125
-    PR3 = 39063;
+    PR3 = 39062;
     TMR3 = 0;   // Reset timer T3 count register
 
     T3CONbits.TON = 1;
